d8b21552048022e18d6006dfc1d64cf17463c3ea
Jean Carlos Trejo <jeancarlostrejo19@gmail.com>
2024-09-21 00:39:36 -0400

    fix error eventshow verify if user is logged for pass variable

--- app/Http/Controllers/EventShowController.php
+++ app/Http/Controllers/EventShowController.php
@@ -7,13 +7,15 @@ use Illuminate\Http\Request;
 
 class EventShowController extends Controller
 {
-    /**
-     * Handle the incoming request.
-     */
     public function __invoke(Event $event)
     {
         $event->load(['country:id,name', 'city:id,name', 'user:id,name,email']);
-        $like = $event->likes()->where('user_id', auth()->user()->id)->first();
+
+        if(auth()->user()) {
+            $like = $event->likes()->where('user_id', auth()->user()->id)->first();
+        } else {
+            $like = null;
+        }
 
         return view('eventsShow', compact('event', 'like'));
     }
